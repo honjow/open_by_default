3655fb56f80da25fcf854ad88a25d8d3e19811be
honjow <honjow311@gmail.com>
2022-01-14 21:03:15 +0800

    format

--- android/src/main/kotlin/com/honjow/open_by_default/OpenByDefaultPlugin.kt
+++ android/src/main/kotlin/com/honjow/open_by_default/OpenByDefaultPlugin.kt
@@ -3,6 +3,10 @@ package com.honjow.open_by_default
 import androidx.annotation.NonNull
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -11,33 +15,35 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** OpenByDefaultPlugin */
-class OpenByDefaultPlugin: FlutterPlugin, MethodCallHandler {
+class OpenByDefaultPlugin : FlutterPlugin, MethodCallHandler {
 
-  private lateinit var channel : MethodChannel
-  private lateinit var applicationContext : Context
+    private lateinit var channel: MethodChannel
+    private lateinit var applicationContext: Context
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    applicationContext = flutterPluginBinding.applicationContext
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "honjow.com/open_by_default/methods")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "openByDefault") {
-      val openByDefaultIntent = Intent()
-      openByDefaultIntent.setAction(android.provider.Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS)
-      openByDefaultIntent.addCategory(Intent.CATEGORY_DEFAULT)
-      openByDefaultIntent.setData(android.net.Uri.parse("package:${applicationContext.packageName}"))
-      openByDefaultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      openByDefaultIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-      openByDefaultIntent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-      applicationContext.startActivity(openByDefaultIntent)
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        applicationContext = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "honjow.com/open_by_default/methods")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "openByDefault") {
+            val openByDefaultIntent = Intent(
+                    Settings.ACTION_APP_OPEN_BY_DEFAULT_SETTINGS,
+                    Uri.parse("package:${applicationContext.packageName}")
+            )
+            openByDefaultIntent.addCategory(Intent.CATEGORY_DEFAULT)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+            applicationContext.startActivity(openByDefaultIntent)
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
